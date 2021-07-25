a811efa774d379603c91b591dcf921b9dc59480a
Artem Pushkin <idfumg@gmail.com>
2021-07-25 11:45:30 +0300

    Add a more concise solution to the arrays/leetcode/003_sum_of_subarray_minimums.cpp

--- arrays/leetcode/003_sum_of_subarray_minimums.cpp
+++ arrays/leetcode/003_sum_of_subarray_minimums.cpp
@@ -21,9 +21,86 @@ int tab(const vi& arr) {
     return ans;
 }
 
+int tab2(const vi& arr) {
+    const int n = arr.size();
+    deque<vi> mq;
+    vi left(n);
+    for (int i = 0; i < n; ++i) {
+        int count = 1;
+        while (not mq.empty() and arr[i] < arr[mq.back()[0]]) {
+            count += mq.back()[1];
+            mq.pop_back();
+        }
+        left[i] = count;
+        mq.push_back({i, count});
+    }
+    mq.clear();
+    vi right(n);
+    for (int i = n - 1; i >= 0; --i) {
+        int count = 1;
+        while (not mq.empty() and arr[i] <= arr[mq.back()[0]]) {
+            count += mq.back()[1];
+            mq.pop_back();
+        }
+        right[i] = count;
+        mq.push_back({i, count});
+    }
+    uint64_t ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += left[i] * right[i] * arr[i];
+        ans %= MOD;
+    }
+    return ans;
+}
+
+int tab3(const vi& arr) {
+    const int n = arr.size();
+    deque<int> mq;
+    vi left(n);
+    for (int i = 0; i < n; ++i) {
+        int count = 1;
+        while (not mq.empty() and arr[i] < arr[mq.back()]) {
+            count += left[mq.back()];
+            mq.pop_back();
+        }
+        left[i] = count;
+        mq.push_back(i);
+    }
+    mq.clear();
+    vi right(n);
+    for (int i = n - 1; i >= 0; --i) {
+        int count = 1;
+        while (not mq.empty() and arr[i] <= arr[mq.back()]) {
+            count += right[mq.back()];
+            mq.pop_back();
+        }
+        right[i] = count;
+        mq.push_back(i);
+    }
+    uint64_t ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += static_cast<uint64_t>(left[i]) * right[i] * arr[i];
+        ans %= MOD;
+    }
+    return ans;
+}
+
 int main() { TimeMeasure _; __x();
     cout << tab({1,2,3,4}) << endl; // 20
     cout << tab({4,3,2,1}) << endl; // 20
     cout << tab({3,1,2,4}) << endl; // 17
     cout << tab({11,81,94,43,3}) << endl; // 444
+    cout << tab({71,55,82,55}) << endl; // 593
+    cout << endl;
+    cout << tab2({1,2,3,4}) << endl; // 20
+    cout << tab2({4,3,2,1}) << endl; // 20
+    cout << tab2({3,1,2,4}) << endl; // 17
+    cout << tab2({11,81,94,43,3}) << endl; // 444
+    cout << tab2({71,55,82,55}) << endl; // 593
+    cout << endl;
+    cout << tab3({1,2,3,4}) << endl; // 20
+    cout << tab3({4,3,2,1}) << endl; // 20
+    cout << tab3({3,1,2,4}) << endl; // 17
+    cout << tab3({11,81,94,43,3}) << endl; // 444
+    cout << tab3({71,55,82,55}) << endl; // 593
 }
