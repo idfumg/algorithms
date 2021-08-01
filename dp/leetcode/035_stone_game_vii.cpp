ca9ccc445cc3b182dc532c922df0c9eb0fe7538a
Artem Pushkin <idfumg@gmail.com>
2021-08-01 18:44:35 +0300

    Update dp/leetcode/035_stone_game_vii.cpp

--- dp/leetcode/035_stone_game_vii.cpp
+++ dp/leetcode/035_stone_game_vii.cpp
@@ -1,13 +1,15 @@
 #include "../../template.hpp"
 
-int rec(const vi& arr, const vi& prefix, const int i, const int j, const int one) {
-    if (i >= j) return 0;
-    if (one) return max(
-        rec(arr, prefix, i + 1, j, 0) + prefix[j] - prefix[i],
-        rec(arr, prefix, i, j - 1, 0) + prefix[j - 1] - prefix[i - 1]);
+int rec(const vi& arr, const vi& prefix, const int i, const int j, const int p) {
+    if (i > j) return 0;
+    if (p == 0) {
+        return max(
+            rec(arr, prefix, i + 1, j, 1 - p) + prefix[j] - prefix[i],
+            rec(arr, prefix, i, j - 1, 1 - p) + prefix[j - 1] - prefix[i - 1]);
+    }
     return min(
-        rec(arr, prefix, i + 1, j, 1) - prefix[j] + prefix[i],
-        rec(arr, prefix, i, j - 1, 1) - prefix[j - 1] + prefix[i - 1]);
+        rec(arr, prefix, i + 1, j, 1 - p) - (prefix[j] - prefix[i]),
+        rec(arr, prefix, i, j - 1, 1 - p) - (prefix[j - 1] - prefix[i - 1]));
 }
 
 int rec(const vi& arr) {
@@ -16,7 +18,7 @@ int rec(const vi& arr) {
     for (int i = 1; i <= n; ++i) {
         prefix[i] = prefix[i - 1] + arr[i - 1];
     }
-    return rec(arr, prefix, 1, n, 1);
+    return rec(arr, prefix, 1, n, 0);
 }
 
 int tab(const vi& arr) {
@@ -25,29 +27,25 @@ int tab(const vi& arr) {
     for (int i = 1; i <= n; ++i) {
         prefix[i] = prefix[i - 1] + arr[i - 1];
     }
-    int idx = 0;
-    vvvi dp(2, vvi(n + 1, vi(2)));
+    vvvi dp(n + 2, vvi(n + 1, vi(2)));
     for (int i = n; i >= 1; --i) {
-        idx = i & 1;
         for (int j = 1; j <= n; ++j) {
-            for (int one : {0, 1}) {
-                if (i >= j) {
-                    dp[idx][j][one] = 0;
-                }
-                else if (one) {
-                    dp[idx][j][1] = max(
-                        dp[1 - idx][j][0] + prefix[j] - prefix[i],
-                        dp[idx][j - 1][0] + prefix[j - 1] - prefix[i - 1]);
+            for (int p : {1, 0}) {
+                if (i > j) dp[i][j][p] = 0;
+                else if (p == 0) {
+                    dp[i][j][p] = max(
+                        dp[i + 1][j][1 - p] + (prefix[j] - prefix[i]),
+                        dp[i][j - 1][1 - p] + (prefix[j - 1] - prefix[i - 1]));
                 }
                 else {
-                    dp[idx][j][0] = min(
-                        dp[1 - idx][j][1] - prefix[j] + prefix[i],
-                        dp[idx][j - 1][1] - prefix[j - 1] + prefix[i - 1]);
+                    dp[i][j][p] = min(
+                        dp[i + 1][j][1 - p] - (prefix[j] - prefix[i]),
+                        dp[i][j - 1][1 - p] - (prefix[j - 1] - prefix[i - 1]));
                 }
             }
         }
     }
-    return dp[idx][n][1];
+    return dp[1][n][0];
 }
 
 int main() { TimeMeasure _; __x();
