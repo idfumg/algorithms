6a8decff6c7c3d23b2ae33c97dd4711c5b205745
Artem Pushkin <idfumg@gmail.com>
2021-08-01 21:00:30 +0300

    Update dp/leetcode/030_longest_palindromic_substring.cpp

--- dp/leetcode/030_longest_palindromic_substring.cpp
+++ dp/leetcode/030_longest_palindromic_substring.cpp
@@ -1,16 +1,18 @@
 #include "../../template.hpp"
 
-bool IsPalindrome(const string& s, const int i, const int j) {
-    if (i >= j) return true;
-    return s[i - 1] == s[j - 1] and IsPalindrome(s, i + 1, j - 1);
+int is_pali(const string& s, const int i, const int j) {
+    if (i > j) return 1;
+    return s[i - 1] == s[j - 1] and is_pali(s, i + 1, j - 1);
 }
 
 int rec(const string& s, const int i, const int j) {
-    if (i > j) return -INF;
+    if (i > j) return 0;
     if (i == j) return 1;
     if (j - i == 1) return s[i - 1] == s[j - 1] ? 2 : 1;
-    if (s[i - 1] == s[j - 1] and IsPalindrome(s, i, j)) return j - i + 1;
-    return max(rec(s, i + 1, j), rec(s, i, j - 1));
+    if (s[i - 1] == s[j - 1] and is_pali(s, i, j)) return j - i + 1;
+    return max(
+        rec(s, i + 1, j),
+        rec(s, i, j - 1));
 }
 
 int rec(const string& s) {
@@ -19,20 +21,30 @@ int rec(const string& s) {
 
 string tab(const string& s) {
     const int n = s.size();
-    int ans = 0;
-    int count = 0;
     vvi pali(n + 2, vi(n + 1));
     for (int i = n; i >= 1; --i) {
         for (int j = 1; j <= n; ++j) {
-            if (i >= j) pali[i][j] = 1;
+            if (i > j) pali[i][j] = 1;
             else pali[i][j] = s[i - 1] == s[j - 1] and pali[i + 1][j - 1];
-            if (pali[i][j] and j - i + 1 > count) {
-                count = j - i + 1;
-                ans = i;
+        }
+    }
+    vvi dp(n + 2, vi(n + 1));
+    int ans = -INF;
+    int idx = 0;
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i > j) dp[i][j] = 0;
+            else if (i == j) dp[i][j] = 1;
+            else if (j - i == 1) dp[i][j] = s[i - 1] == s[j - 1] ? 2 : 1;
+            else if (s[i - 1] == s[j - 1] and pali[i][j]) dp[i][j] = j - i + 1;
+            else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            if (ans < dp[i][j]) {
+                ans = dp[i][j];
+                idx = i;
             }
         }
     }
-    return s.substr(ans - 1, count);
+    return s.substr(idx - 1, ans);
 }
 
 int main() { TimeMeasure _; __x();
