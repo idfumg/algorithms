dcbb79a024c7074574bf5c65f26b1d4d149cbadb
Artem Pushkin <idfumg@gmail.com>
2021-08-01 18:44:56 +0300

    Update dp/leetcode/036_stone_game_iii.cpp

--- dp/leetcode/036_stone_game_iii.cpp
+++ dp/leetcode/036_stone_game_iii.cpp
@@ -1,59 +1,74 @@
 #include "../../template.hpp"
 
-int rec(const vi& arr, const int n, const int one) {
-    if (n >= arr.size()) return 0;
-    if (one == 1) {
-        int ans = -INF;
+int rec(const vi& arr, const int n, const int p, const int m) {
+    if (n > arr.size() - 1) return 0;
+    if (p == 0) {
         int sum = 0;
-        for (int k = 1; k <= 3 and n + k - 1 < arr.size(); ++k) {
-            sum += arr[n + k - 1];
-            ans = max(ans, sum + rec(arr, n + k, 0));
+        int ans = -INF;
+        for (int k = 1; k <= m; ++k) {
+            if (n + k - 1 < arr.size()) {
+                sum += arr[n + k - 1];
+                ans = max(ans, rec(arr, n + k, 1 - p, m) + sum);
+            }
         }
         return ans;
     }
-    int ans = INF;
     int sum = 0;
-    for (int k = 1; k <= 3 and n + k - 1 < arr.size(); ++k) {
-        sum += arr[n + k - 1];
-        ans = min(ans, -sum + rec(arr, n + k, 1));
+    int ans = INF;
+    for (int k = 1; k <= m; ++k) {
+        if (n + k - 1 < arr.size()) {
+            sum += arr[n + k - 1];
+            ans = min(ans, rec(arr, n + k, 1 - p, m) - sum);
+        }
     }
     return ans;
 }
 
 string rec(const vi& arr) {
-    const int res = rec(arr, 0, 1);
-    if (res == 0) return "Tie";
-    return res > 0 ? "Alice" : "Bob";
+    const int ans = rec(arr, 0, 0, 3);
+    return ans == 0
+        ? "Tie"
+        : ans > 0
+            ? "Alice"
+            : "Bob";
 }
 
 string tab(const vi& arr) {
     const int n = arr.size();
-    vvi dp(n + 2, vi(2));
+    const int m = 3;
+    vvi dp(n + 1, vi(2));
     for (int i = n - 1; i >= 0; --i) {
-        for (int one : {0, 1}) {
-            if (one == 1) {
-                int ans = -INF;
+        for (int p : {1, 0}) {
+            if (p == 0) {
                 int sum = 0;
-                for (int k = 1; k <= 3 and i + k - 1 < n; ++k) {
-                    sum += arr[i + k - 1];
-                    ans = max(ans, sum + dp[i + k][0]);
+                int ans = -INF;
+                for (int k = 1; k <= m; ++k) {
+                    if (i + k - 1 < n) {
+                        sum += arr[i + k - 1];
+                        ans = max(ans, dp[i + k][1 - p] + sum);
+                    }
                 }
-                dp[i][one] = ans;
+                dp[i][p] = ans;
             }
             else {
-                int ans = INF;
                 int sum = 0;
-                for (int k = 1; k <= 3 and i + k - 1 < n; ++k) {
-                    sum += arr[i + k - 1];
-                    ans = min(ans, -sum + dp[i + k][1]);
+                int ans = INF;
+                for (int k = 1; k <= m; ++k) {
+                    if (i + k - 1 < n) {
+                        sum += arr[i + k - 1];
+                        ans = min(ans, dp[i + k][1 - p] - sum);
+                    }
                 }
-                dp[i][one] = ans;
+                dp[i][p] = ans;
             }
         }
     }
-    const int res = dp[0][1];
-    if (res == 0) return "Tie";
-    return res > 0 ? "Alice" : "Bob";
+    const int ans = dp[0][0];
+    return ans == 0
+        ? "Tie"
+        : ans > 0
+            ? "Alice"
+            : "Bob";
 }
 
 int main() { TimeMeasure _; __x();
