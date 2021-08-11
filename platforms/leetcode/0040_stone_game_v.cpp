f53e130b47786a157be137dc0b17e74df3687926
Artem Pushkin <idfumg@gmail.com>
2021-08-11 22:07:22 +0300

    Update platforms/leetcode/0040_stone_game_v.cpp

--- platforms/leetcode/0040_stone_game_v.cpp
+++ platforms/leetcode/0040_stone_game_v.cpp
@@ -1,17 +1,16 @@
 #include "../../template.hpp"
 
-int rec(const vi& arr, const vi& prefix, const int i, const int j, vvi& dp) {
+int rec(const vi& arr, const vi& prefix, const int i, const int j) {
     if (i >= j) return 0;
-    if (dp[i][j] != -1) return dp[i][j];
-    int ans = 0;
-    for (int k = i; k <= j; ++k) {
+    int ans = -INF;
+    for (int k = i; k < j; ++k) {
         const int left = prefix[k] - prefix[i - 1];
         const int right = prefix[j] - prefix[k];
-        if (left > right) ans = max(ans, rec(arr, prefix, k + 1, j, dp) + right);
-        else if (right > left) ans = max(ans, rec(arr, prefix, i, k, dp) + left);
-        else ans = max(ans, max(rec(arr, prefix, k + 1, j, dp), rec(arr, prefix, i, k, dp)) + left);
+        if (left > right) ans = max(ans, right + rec(arr, prefix, k + 1, j));
+        else if (right > left) ans = max(ans, left + rec(arr, prefix, i, k));
+        else ans = max(ans, left + max(rec(arr, prefix, i, k), rec(arr, prefix, k + 1, j)));
     }
-    return dp[i][j] = ans;
+    return ans;
 }
 
 int rec(const vi& arr) {
@@ -20,30 +19,29 @@ int rec(const vi& arr) {
     for (int i = 1; i <= n; ++i) {
         prefix[i] = prefix[i - 1] + arr[i - 1];
     }
-    vvi dp(n + 2, vi(n + 1, -1));
-    return rec(arr, prefix, 1, n, dp);
+    return rec(arr, prefix, 1, n);
 }
 
 int tab(const vi& arr) {
     const int n = arr.size();
-    vi prefix(arr.size() + 1);
-    for (int i = 1; i <= arr.size(); ++i) {
+    vi prefix(n + 1);
+    for (int i = 1; i <= n; ++i) {
         prefix[i] = prefix[i - 1] + arr[i - 1];
     }
-    vvi dp(n + 2, vi(n + 1));
+    vvi dp(n + 1, vi(n + 2));
     for (int i = n; i >= 1; --i) {
         for (int j = 1; j <= n; ++j) {
             if (i >= j) {
                 dp[i][j] = 0;
             }
             else {
-                int ans = 0;
+                int ans = -INF;
                 for (int k = i; k < j; ++k) {
                     const int left = prefix[k] - prefix[i - 1];
                     const int right = prefix[j] - prefix[k];
-                    if (left > right) ans = max(ans, dp[k + 1][j] + right);
-                    else if (right > left) ans = max(ans, dp[i][k] + left);
-                    else ans = max(ans, max(dp[k + 1][j], dp[i][k]) + left);
+                    if (left > right) ans = max(ans, right + dp[k + 1][j]);
+                    else if (right > left) ans = max(ans, left + dp[i][k]);
+                    else ans = max(ans, max(dp[i][k], dp[k + 1][j]) + left);
                 }
                 dp[i][j] = ans;
             }
@@ -57,7 +55,7 @@ int main() { TimeMeasure _; __x();
     cout << rec({7,7,7,7,7,7,7}) << endl; // 28
     cout << rec({4}) << endl; // 0
     cout << rec({10,9,8,7,6,5,4,3,2,1}) << endl; // 37
-    cout << rec({2,1,1}) << endl; // 0
+    cout << rec({2,1,1}) << endl; // 3
     cout << endl;
     cout << tab({6,2,3,4,5,5}) << endl; // 18
     cout << tab({7,7,7,7,7,7,7}) << endl; // 28
