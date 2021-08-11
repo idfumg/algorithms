758d94e7c3a181c6ac5eb60aa877e9083e34a14e
Artem Pushkin <idfumg@gmail.com>
2021-08-11 22:07:48 +0300

    Update platforms/leetcode/0079_longest_arithmetic_subsequence_of_given_difference.cpp

--- platforms/leetcode/0079_longest_arithmetic_subsequence_of_given_difference.cpp
+++ platforms/leetcode/0079_longest_arithmetic_subsequence_of_given_difference.cpp
@@ -12,6 +12,6 @@ int tab(const vi& arr, const int k) {
 int main() { TimeMeasure _; __x();
     cout << tab({1,2,3,4}, 1) << endl; // 4
     cout << tab({1,3,5,7}, 1) << endl; // 1
-    cout << tab({1,5,7,8,5,3,4,2,1}, -2) << endl; // 4
+    cout << tab({1,5,7,8,5,3,4,2,1}, 2) << endl; // 2
     cout << tab({1,5,7,8,5,3,4,2,1}, -2) << endl; // 4
 }
