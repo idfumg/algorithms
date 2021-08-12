f35c2e6b6640a516d62974a1c747665d4d1a8c9e
Artem Pushkin <idfumg@gmail.com>
2021-08-12 21:35:34 +0300

    Update platforms/leetcode/0044_sum_of_subarray_minimums.cpp

--- platforms/leetcode/0044_sum_of_subarray_minimums.cpp
+++ platforms/leetcode/0044_sum_of_subarray_minimums.cpp
@@ -88,6 +88,31 @@ uint64_t tab3(const vi& arr) {
     return ans;
 }
 
+int tab4(const vi& arr) {
+    const int n = arr.size();
+    deque<int> mq;
+    vi left(n);
+    for (int i = 0; i < n; ++i) {
+        while (not mq.empty() and arr[i] < arr[mq.back()]) mq.pop_back();
+        if (mq.empty()) left[i] = 0;
+        else left[i] = mq.back() + 1;
+        mq.push_back(i);
+    }
+    mq.clear();
+    vi right(n);
+    for (int i = n - 1; i >= 0; --i) {
+        while (not mq.empty() and arr[i] <= arr[mq.back()]) mq.pop_back();
+        if (mq.empty()) right[i] = n - 1;
+        else right[i] = mq.back() - 1;
+        mq.push_back(i);
+    }
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += arr[i] * (i - left[i] + 1) * (right[i] - i + 1);
+    }
+    return ans;
+}
+
 int main() { TimeMeasure _; __x();
     cout << tab({1,2,3,4}) << endl; // 20
     cout << tab({4,3,2,1}) << endl; // 20
@@ -106,4 +131,10 @@ int main() { TimeMeasure _; __x();
     cout << tab3({3,1,2,4}) << endl; // 17
     cout << tab3({11,81,94,43,3}) << endl; // 444
     cout << tab3({71,55,82,55}) << endl; // 593
+    cout << endl;
+    cout << tab4({1,2,3,4}) << endl; // 20
+    cout << tab4({4,3,2,1}) << endl; // 20
+    cout << tab4({3,1,2,4}) << endl; // 17
+    cout << tab4({11,81,94,43,3}) << endl; // 444
+    cout << tab4({71,55,82,55}) << endl; // 593
 }
