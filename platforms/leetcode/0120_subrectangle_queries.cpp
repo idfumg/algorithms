b73434bd052b4dd2ec93c9139f70fd4ae5fdc0c5
Artem Pushkin <idfumg@gmail.com>
2021-08-22 19:32:53 +0300

    Add platforms/leetcode/0120_subrectangle_queries.cpp

---
+++ platforms/leetcode/0120_subrectangle_queries.cpp
@@ -0,0 +1,21 @@
+class SubrectangleQueries {
+private:
+    vector<vector<int>>& arr;
+
+public:
+    SubrectangleQueries(vector<vector<int>>& rectangle) : arr(rectangle) {
+
+    }
+
+    void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; ++i) {
+            for (int j = col1; j <= col2; ++j) {
+                arr[i][j] = newValue;
+            }
+        }
+    }
+
+    int getValue(int row, int col) {
+        return arr[row][col];
+    }
+};
