750a52a9104d5328a2b26602a53552794c0ebbbc
Artem Pushkin <idfumg@gmail.com>
2021-08-22 19:31:34 +0300

    Update collection/trie.cpp

--- collection/trie.cpp
+++ collection/trie.cpp
@@ -1,77 +1,50 @@
-#include "../template.hpp"
+#include "../../template.hpp"
 
-struct TrieNode {
-    unordered_map<char, unique_ptr<TrieNode>> children;
-};
+struct Trie {
+    using Characters = array<int, 27>;
+    vector<Characters> kids = vector<Characters>(1, Characters{});
 
-class Trie {
-private:
-    TrieNode root;
-
-private:
-    TrieNode const* find(const string& s) const {
-        TrieNode const* node = &root;
-        for (const char ch : s) {
-            if (not node->children.count(ch)) {
-                return nullptr;
-            }
-            node = node->children.at(ch).get();
-        }
-        return node;
-    }
-
-    template<class Fn>
-    void apply_(const TrieNode* const node, string& word, Fn&& fn) const {
-        if (node->children.count('\0')) {
-            fn(word);
-        }
-        for (const auto& [ch, child] : node->children) {
-            if (ch != '\0') {
-                word.push_back(ch);
-                apply_(child.get(), word, fn);
-                word.pop_back();
-            }
-        }
-    }
-
-public:
     void insert(const string& s) {
-        TrieNode* node = &root;
+        int node = 0;
         for (const char ch : s) {
-            if (not node->children.count(ch)) {
-                node->children[ch] = make_unique<TrieNode>();
+            const int idx = ch - 'a';
+            if (kids[node][idx] == 0) {
+                kids.push_back(Characters{});
+                kids[node][idx] = kids.size() - 1;
             }
-            node = node->children[ch].get();
+            node = kids[node][idx];
         }
-        node->children['\0'];
+        kids[node][26] = 1;
     }
 
-    bool contains(const string& word) const {
-        const auto* node = find(word);
-        return node and node->children.count('\0');
+    bool search(const string& s) {
+        int node = 0;
+        for (const char ch : s) {
+            const int idx = ch - 'a';
+            if (kids[node][idx] == 0) return false;
+            node = kids[node][idx];
+        }
+        return kids[node][26];
     }
 
-    template<class Fn>
-    void apply(string prefix, Fn&& fn) const {
-        if (const auto* node = find(prefix)) {
-            apply_(node, prefix, fn);
+    bool starts_with(const string& s) {
+        int node = 0;
+        for (const char ch : s) {
+            const int idx = ch - 'a';
+            if (kids[node][idx] == 0) return false;
+            node = kids[node][idx];
         }
+        return true;
     }
 };
 
-int main() { TimeMeasure _;
+int main() { TimeMeasure _; //__x();
     Trie trie;
-
     trie.insert("apple");
+    cout << trie.search("apple") << endl; // 1
+    cout << trie.search("app") << endl; // 0
+    cout << trie.starts_with("app") << endl; // 1
+    cout << trie.starts_with("apr") << endl; // 0
     trie.insert("app");
-    trie.insert("banana");
-    trie.insert("appalled");
-
-    trie.insert("appalachians");
-    cout << trie.contains("appalachians") << endl; // 1
-    cout << trie.contains("appalachian") << endl; // 0
-    trie.insert("appalachian");
-    cout << trie.contains("appalachian") << endl; // 1
-
-    trie.apply("app", [](std::string const& w) { std::cout << w << '\n'; });
+    cout << trie.search("app") << endl; // 1
 }
