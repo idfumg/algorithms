fd3f3a3b165866b92f45ef5dd19b4931c1d76352
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/0077_remove_k_digits.cpp

--- platforms/leetcode/0077_remove_k_digits.cpp
+++ platforms/leetcode/0077_remove_k_digits.cpp
@@ -1,32 +1,63 @@
 #include "../../template.hpp"
 
-int rec(const int n, const int k) {
-    if (n == 0) return 0;
-    if (k > 0) return min(
-        rec(n / 10, k) * 10 + (n % 10),
-        rec(n / 10, k - 1));
-    return rec(n / 10, 0) * 10 + (n % 10);
+void rec(string s, int n, int k, string& elems, string& ans) {
+    if (n < 0 and k != 0) return;
+    if (n < 0 and k == 0) {
+        if (not elems.empty()) {
+            string temp = elems;
+            while (temp.size() > 1 and temp.back() == '0') temp.pop_back();
+            reverse(temp.begin(), temp.end());
+
+            if (ans.size() < elems.size()) ans = temp;
+            else if (ans.size() == elems.size() and ans > temp) ans = temp;
+        }
+        return;
+    }
+
+    rec(s, n - 1, k - 1, elems, ans);
+    elems += s[n];
+    rec(s, n - 1, k, elems, ans);
+    elems.pop_back();
 }
 
-string rec(const string& s, const int k) {
-    const int n = stoi(s);
-    const int res = rec(n, k);
-    const string ans = to_string(res);
-    return ans;
+string rec(string s, int k) {
+    string elems;
+    string ans;
+    rec(s, s.size() - 1, k, elems, ans);
+    return ans.empty() ? "0" : ans;
 }
 
-string tab(const string& s, int k) {
-    const int n = s.size();
+int rec2(int num, int k) {
+    if (k < 0) return INF;
+    if (num == 0) return 0;
+    return min(
+        rec2(num / 10, k - 1),
+        rec2(num / 10, k) * 10 + (num % 10));
+}
+
+string rec2(string s, int k) {
+    int num = stoi(s);
+    int ans = rec2(num, k);
+    return to_string(ans);
+}
+
+string tab(string s, int k) {
+    int n = s.size();
+
     deque<int> mq;
     for (int i = 0; i < n; ++i) {
-        while (not mq.empty() and s[i] < s[mq.back()] and k-- > 0) mq.pop_back();
+        while (not mq.empty() and s[i] < s[mq.back()] and k-- > 0) {
+            mq.pop_back();
+        }
         mq.push_back(i);
     }
+
+    while (k-- > 0 and not mq.empty()) mq.pop_back();
     while (not mq.empty() and s[mq.front()] == '0') mq.pop_front();
-    while (not mq.empty() and k-- > 0) mq.pop_back();
+
     string ans;
-    for (int i = 0; i < mq.size(); ++i) {
-        ans += s[mq[i]];
+    for (int i : mq) {
+        ans += s[i];
     }
     return ans.empty() ? "0" : ans;
 }
@@ -38,6 +69,15 @@ int main() { TimeMeasure _; __x();
     cout << rec("9", 1) << endl; // 0
     cout << rec("91", 1) << endl; // 1
     cout << rec("19", 1) << endl; // 1
+    cout << rec("10001", 4) << endl; // 0
+    cout << endl;
+    cout << rec2("1432219", 3) << endl; // 1219
+    cout << rec2("10200", 3) << endl; // 0
+    cout << rec2("10", 2) << endl; // 0
+    cout << rec2("9", 1) << endl; // 0
+    cout << rec2("91", 1) << endl; // 1
+    cout << rec2("19", 1) << endl; // 1
+    cout << rec2("10001", 4) << endl; // 0
     cout << endl;
     cout << tab("1432219", 3) << endl; // 1219
     cout << tab("10200", 3) << endl; // 0
@@ -45,4 +85,5 @@ int main() { TimeMeasure _; __x();
     cout << tab("9", 1) << endl; // 0
     cout << tab("91", 1) << endl; // 1
     cout << tab("19", 1) << endl; // 1
+    cout << tab("10001", 4) << endl; // 0
 }
