410b5f6d502cc47e742a8c32dc30374ceda69dc6
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update template.hpp

--- template.hpp
+++ template.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <bits/stdc++.h>
+#include "template_tree.hpp"
 
 using namespace std;
-
-#define endl '\n'
 
 constexpr double PI = 3.1415926535897932384626433832795;
 constexpr double EPS = 0.0000000001;
@@ -13,187 +12,101 @@ constexpr int INF = 1000000000 + 7;
 constexpr int null = -INF;
 constexpr int MOD = 1000000000 + 7;
 
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vvvi = vector<vvi>;
-using vvvvi = vector<vvvi>;
-using vb = vector<bool>;
-using vvb = vector<vb>;
-using vvvb = vector<vvb>;
-using vc = vector<char>;
-using vvc = vector<vc>;
-using vs = vector<string>;
-using vvs = vector<vs>;
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+using vvvi = std::vector<vvi>;
+using vvvvi = std::vector<vvvi>;
+using vb = std::vector<bool>;
+using vvb = std::vector<vb>;
+using vvvb = std::vector<vvb>;
+using vc = std::vector<char>;
+using vvc = std::vector<vc>;
+using vs = std::vector<string>;
+using vvs = std::vector<vs>;
 
-using ld = long double;
-using ll = int64_t;
-using ull = uint64_t;
-using vd = vector<double>;
-using cvi = const vector<int>&;
-using vll = vector<ll>;
-using pi = pair<int, int>;
-using cpi = const pair<int, int>&;
-using vpi = vector<pi>;
-using cvpi = const vector<pi>&;
-using vvpi = vector<vpi>;
-using cvvpi = const vector<vpi>&;
-using vvvpi = vector<vvpi>;
-using vvvvpi = vector<vvvpi>;
-using cvvi = const vector<vi>&;
-using vvd = vector<vd>;
-using cvvvi = const vector<vvi>&;
-using vvll = vector<vll>;
-using vvvll = vector<vvll>;
-using cvb = const vector<bool>&;
-using cvvb = const vector<vb>&;
-using si = set<int>;
-using csi = const set<int>&;
-using sll = set<ll>;
-using qi = queue<int>;
-using qll = queue<ll>;
-using di = deque<int>;
-using dll = deque<ll>;
-
-ll mod(const ll x) noexcept {
-    return (x % MOD + MOD) % MOD;
-}
+using ll = std::int64_t;
+using ull = std::uint64_t;
 
 template<class T>
-static ostream& operator << (ostream& os, const pair<T, T>& p) noexcept {
+std::ostream& operator << (std::ostream& os, const std::pair<T, T>& p) noexcept {
     return os << p.first << ',' << p.second;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const vector<pair<T, T>>& v) noexcept {
+std::ostream& operator << (std::ostream& os, const std::vector<std::pair<T, T>>& v) noexcept {
     for (const auto& value : v) os << '{' << value.first << ',' << value.second << '}' << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const deque<T>& v) noexcept {
+std::ostream& operator << (std::ostream& os, const std::deque<T>& v) noexcept {
     for (const auto& value : v) os << value << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const vector<T>& v) noexcept {
+std::ostream& operator << (std::ostream& os, const std::vector<T>& v) noexcept {
     for (const auto& value : v) os << value << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const vector<vector<T>>& v) noexcept {
+std::ostream& operator << (std::ostream& os, const std::vector<vector<T>>& v) noexcept {
     os << '\n';
     for (const auto& L : v) os << L << endl;
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const multiset<T>& s) noexcept {
+std::ostream& operator << (std::ostream& os, const std::multiset<T>& s) noexcept {
     for (const auto& value : s) os << value << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const set<T>& s) noexcept {
+std::ostream& operator << (std::ostream& os, const std::set<T>& s) noexcept {
     for (const auto& value : s) os << value << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const unordered_set<T>& s) noexcept {
+std::ostream& operator << (std::ostream& os, const std::unordered_set<T>& s) noexcept {
     for (const auto& value : s) os << value << ' ';
-    return os;// << endl;
+    return os;
 }
 
 template<class T, class U>
-static ostream& operator << (ostream& os, const map<T, U>& tab) noexcept {
-    os << '\n';
+std::ostream& operator << (std::ostream& os, const std::map<T, U>& tab) noexcept {
     for (const auto& [key, value] : tab) os << key << ": " << value << '\n';
-    return os;// << endl;
+    return os;
 }
 
 template<class T, class U>
-static ostream& operator << (ostream& os, const unordered_map<T, U>& tab) noexcept{
-    os << '\n';
+std::ostream& operator << (std::ostream& os, const std::unordered_map<T, U>& tab) noexcept {
     for (const auto& [key, value] : tab) os << key << ": " << value << '\n';
-    return os;// << endl;
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const stack<T>& v) noexcept {
-    auto temp = v;
-    while (not temp.empty()) {
-        os << temp.top();
-        temp.pop();
-    }
-    return os;// << endl;
+std::ostream& operator << (std::ostream& os, const std::stack<T>& v) noexcept {
+    for (auto temp = v; not temp.empty(); temp.pop()) os << temp.top() << ' ';
+    return os;
 }
 
 template<class T>
-static ostream& operator << (ostream& os, const queue<T>& v) noexcept {
-    auto temp = v;
-    while (not temp.empty()) {
-        os << temp.front() << ' ';
-        temp.pop();
-    }
-    return os;// << endl;
+std::ostream& operator << (std::ostream& os, const std::queue<T>& v) noexcept {
+    for (auto temp = v; not temp.empty(); temp.pop()) os << temp.front() << ' ';
+    return os;
 }
 
 template<class ... T>
-static ostream& operator << (ostream& os, const priority_queue<T...>& v) noexcept {
-    auto temp = v;
-    while (not temp.empty()) {
-        os << temp.top() << ' ';
-        temp.pop();
-    }
-    return os;// << endl;
+std::ostream& operator << (std::ostream& os, const std::priority_queue<T...>& v) noexcept {
+    for (auto temp = v; not temp.empty(); temp.pop()) os << temp.top() << ' ';
+    return os;
 }
-
-template<class T>
-static istream& operator >> (istream& is, set<T>& s) noexcept {
-    while (is) {
-        T value; is >> value;
-        s.insert(value);
-    }
-    return is;
-}
-
-template<class T>
-static istream& operator >> (istream& is, vector<T>& v) noexcept {
-    for (size_t i = 0; i < v.size(); ++i) is >> v[i];
-    return is;
-}
-
-template<class T>
-static istream& operator >> (istream& is, vector<vector<int>>& vv) noexcept {
-    for (size_t i = 0; i < vv.size(); ++i) is >> vv[i];
-    return is;
-}
-
-template<class T, class U>
-static istream& operator >> (istream& is, unordered_map<T, U>& tab) noexcept {
-    while (is) {
-        T key; U value; is >> key >> value;
-        tab[key] = value;
-    }
-    return is;
-}
-
-template<class T, class U>
-static istream& operator >> (istream& is, map<T, U>& tab) noexcept {
-    while (is) {
-        T key; U value; is >> key >> value;
-        tab[key] = value;
-    }
-    return is;
-}
-
-#define debug(VarName) {cerr << "'" << #VarName << "': " << boolalpha << (VarName) << ' ';}
-#define debugn(VarName) {cerr << "'" << #VarName << "': " << boolalpha << (VarName) << '\n';}
 
 class TimeMeasure final {
-    time_t start;
+    std::time_t start;
 
 public:
     TimeMeasure() noexcept
@@ -202,233 +115,117 @@ public:
 
     }
     ~TimeMeasure() noexcept {
-        cerr << "\nExecution time: " << ms() << " ms.\n";
+        std::cerr << "\nExecution time: " << ms() << " ms.\n";
     }
-    ld ms() const noexcept {
+    double ms() const noexcept {
         const auto stop = clock();
-        const auto elapsed = static_cast<ld>(stop) - start;
+        const auto elapsed = static_cast<double>(stop) - start;
         const auto ns = elapsed / CLOCKS_PER_SEC;
-        const auto ms = ns * static_cast<ld>(1000.0);
+        const auto ms = ns * static_cast<double>(1000.0);
         return ms;
     }
-    friend ostream& operator<<(ostream& os, const TimeMeasure& tm) {
+    friend std::ostream& operator<<(std::ostream& os, const TimeMeasure& tm) {
         return os << "\nElapsed time: " << tm.ms() << " ms.\n";
     }
 };
 
-ll power(ll a, ll p, const ll mod) noexcept {
-    ll res = 1;
-    while (p) {
-        if (p & 1) res = (res * a) % mod;
-        a = (a * a) % mod;
-        p /= 2;
+int power(int base, int p) { // O(logn) // binary exponentiation
+    int res = 1;
+    while (p > 0) {
+        if (p & 1) res *= base, --p;
+        else base *= base, p /= 2;
     }
-    return res % mod;
+    return res;
 }
 
-ll power(ll a, ll p) noexcept {
-    ll res = 1;
-    while (p) {
-        if (p & 1) res *= a;
-        a *= a;
-        p /= 2;
+int power_mod(int base, int p, const int mod) { // O(logn)
+    int res = 1;
+    while (p > 0) {
+        if (p & 1) res *= base, res %= mod, --p;
+        else base *= base, base %= mod, p /= 2;
     }
     return res;
 }
 
 template<class T>
-T sort(T arr) {
-    sort(arr.begin(), arr.end());
-    return arr;
-}
-
-template<class T>
-T reverse(T arr) {
-    reverse(arr.begin(), arr.end());
-    return arr;
-}
-
-template<template<class...> class T, class ... U>
-decay_t<decltype(*T<U...>().begin())> max(const T<U...>& elems) {
-    return *max_element(elems.begin(), elems.end());
-}
-
-template<template<class...> class T, class ... U>
-decay_t<decltype(*T<U...>().begin())> min(const T<U...>& elems) {
-    return *min_element(elems.begin(), elems.end());
-}
-
-template<template<class...> class T, class ... U>
-vector<decay_t<decltype(*T<U...>().begin())>> minmax(const T<U...>& elems) {
-    return {max(elems), min(elems)};
-}
-
-template<class T>
-T min(const T& param) {
-    return param;
-}
-
-template<class T, class ... Args>
-T min(const T& param, const Args& ...args) {
-    return min(param, min(args...));
-}
-
-template<class T>
-T max(const T& param) {
-    return param;
-}
-
-template<class T, class ... Args>
-T max(const T& param, const Args& ...args) {
-    return max(param, max(args...));
-}
-
-template<class T>
-void ruffle_sort(T& arr) { // qsort O(n^2) has complexity if elements sorted
+void ruffle_sort(T& arr) { // qsort O(n^2) has complexity if elements are sorted
     random_shuffle(arr.begin(), arr.end());
     sort(arr.begin(), arr.end());
 }
 
-template <typename T,
-          typename TIter = decltype(std::begin(std::declval<T>())),
-          typename = decltype(std::end(std::declval<T>()))>
-constexpr auto enumerate(T && iterable) noexcept
-{
-    struct iterator
-    {
-        size_t i;
-        TIter iter;
-        bool operator != (const iterator & other) const noexcept { return iter != other.iter; }
-        iterator& operator ++ () noexcept { ++i; ++iter; return *this; }
-        auto operator * () const noexcept { return std::tie(i, *iter); }
-    };
-    struct iterable_wrapper
-    {
-        T& iterable;
-        auto begin() noexcept { return iterator{ 0, std::begin(iterable) }; }
-        auto end() noexcept { return iterator{ 0, std::end(iterable) }; }
-    };
-    return iterable_wrapper{ std::forward<T>(iterable) };
-}
+#define _0_9 "0123456789"
+#define _A_Z "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define _a_z "abcdefghijklmnopqrstuvwxyz"
+#define _ALNUMS _0_9 _A_Z _a_z
 
-vi operator+(const vi& a, const vi& b) {
-    vi ans = a;
-    for_each(b.begin(), b.end(), [&ans](int v){ans.push_back(v);});
-    return ans;
-}
-
-vi operator+(const vi& a, int value) {
-    vi ans = a;
-    ans.push_back(value);
-    return ans;
+template<class T>
+T random_number(const T from, const T to) {
+    std::random_device random_device;
+    std::mt19937_64 generator(random_device());
+    std::uniform_int_distribution<T> distribution{from, to};
+    return distribution(generator);
 }
 
 template<class T>
-set<T> operator+(const set<T>& a, int value) {
-    set<T> ans = a;
-    ans.insert(value);
-    return ans;
-}
-
-struct Node {
-    int value = INF;
-    int val = INF;
-    Node* left = nullptr;
-    Node* right = nullptr;
-    Node* next = nullptr;
-    Node* parent = nullptr;
-    vector<Node*> children = {};
-    Node(const int value) : value(value), val(value), left(nullptr), right(nullptr), next(nullptr) {}
-    Node(const int value, Node* left, Node* right) : value(value), left(left), right(right), next(right) {}
-};
-
-using TreeNode = Node;
-using ListNode = Node;
-
-void print_inorder(Node* root) {
-    if (not root) return;
-    print_inorder(root->left);
-    cout << root->value << ' ';
-    print_inorder(root->right);
-}
-
-void print_preorder(Node* root) {
-    if (not root) return;
-    cout << root->value << ' ';
-    print_preorder(root->left);
-    print_preorder(root->right);
-}
-void print_postorder(Node* root) {
-    if (not root) return;
-    print_postorder(root->left);
-    print_postorder(root->right);
-    cout << root->value << ' ';
-}
-
-Node* find_tree_node(Node* root, const int val) {
-    if (not root) return nullptr;
-    if (root->val == val) {
-        return root;
+std::vector<T> random_numbers(const std::size_t n, const T from, const T to) {
+    std::random_device random_device;
+    std::mt19937_64 generator(random_device());
+    std::uniform_int_distribution<T> distribution{from, to};
+    std::vector<T> numbers(n);
+    for (std::size_t i = 0; i < n; ++i) {
+        numbers[i] = distribution(generator);
     }
-    Node* left = find_tree_node(root->left, val);
-    if (left) return left;
-    return find_tree_node(root->right, val);
+    return numbers;
 }
 
-TreeNode* TreeFromLevelOrder(const vi& arr) {
-    if (arr.empty()) return nullptr;
-    TreeNode* root = new TreeNode(arr[0]);
-    deque<TreeNode*> q;
-    q.push_back(root);
-    int idx = 1;
-    int n = arr.size();
-    while (not q.empty()) {
-        TreeNode* at = q.front();
-        q.pop_front();
-        if (idx < n) {
-            at->left = (arr[idx] == -INF) ? nullptr : (new TreeNode(arr[idx]));
-            ++idx;
-            if (at->left) {
-                q.push_back(at->left);
-                at->left->parent = at;
-            }
-        }
-        if (idx < n) {
-            at->right = (arr[idx] == -INF) ? nullptr : (new TreeNode(arr[idx]));
-            ++idx;
-            if (at->right) {
-                q.push_back(at->right);
-                at->right->parent = at;
-            }
+template<class T>
+std::vector<std::vector<T>> random_matrix(const std::size_t m, const std::size_t n, const T from, const T to) {
+    std::random_device random_device;
+    std::mt19937_64 generator(random_device());
+    std::uniform_int_distribution<T> distribution{from, to};
+    std::vector<std::vector<T>> matrix(m, std::vector<T>(n));
+    for (std::size_t i = 0; i < m; ++i) {
+        for (std::size_t j = 0; j < n; ++j) {
+            matrix[i][j] = distribution(generator);
         }
     }
-    return root;
+    return matrix;
 }
 
-Node* make_list(const vi& arr) {
-    Node* head = nullptr;
-    Node* current = nullptr;
-    for (int value : arr) {
-        if (head == nullptr) {
-            current = head = new Node(value);
-        } else {
-            current->right = current->next = new Node(value);
-            current = current->right;
+std::vector<std::vector<char>> random_matrix(const std::size_t m, const std::size_t n, const std::string_view& chars) {
+    std::random_device random_device;
+    std::mt19937_64 generator(random_device());
+    std::uniform_int_distribution<std::size_t> distribution{0, chars.size() - 1};
+    std::vector<std::vector<char>> matrix(m, std::vector<char>(n));
+    for (std::size_t i = 0; i < m; ++i) {
+        for (std::size_t j = 0; j < n; ++j) {
+            matrix[i][j] = chars[distribution(generator)];
         }
     }
-    return head;
+    return matrix;
 }
 
-void print_list(Node* head) {
-    for (Node* curr = head; curr; curr = curr->right) cout << curr->value << ' ';
-    cout << endl;
+std::string random_string(const std::size_t n, const std::string_view& chars) {
+    std::random_device random_device;
+    std::mt19937_64 generator(random_device());
+    std::uniform_int_distribution<std::size_t> distribution{0, chars.size() - 1};
+    std::string s(n, ' ');
+    for (std::size_t i = 0; i < n; ++i) {
+        s[i] = chars[distribution(generator)];
+    }
+    return s;
 }
 
-void print_list2(Node* head) {
-    for (Node* curr = head; curr; curr = curr->next) cout << curr->value << ' ';
-    cout << endl;
+std::vector<std::string> random_strings(const std::size_t m, const std::size_t n, const std::string_view& chars) {
+    std::vector<std::string> strings;
+    for (std::size_t i = 0; i < m; ++i) {
+        strings.push_back(random_string(n, chars));
+    }
+    return strings;
 }
 
-#define trace() cout << "Line: " << __LINE__ << endl;
+#define debug(VarName) {std::cerr << "'" << #VarName << "': " << std::boolalpha << (VarName) << ' ';}
+#define debugn(VarName) {std::cerr << "'" << #VarName << "': " << std::boolalpha << (VarName) << std::endl;}
+#define trace(Text) std::cout << "Line: " << __LINE__ << " [" << Text << "]" << std::endl;
 
-[[maybe_unused]] static const auto __x = [](){ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cout.precision(15); cout.setf(ios::boolalpha);};
+const int fastio = ([](){std::ios_base::sync_with_stdio(0); std::cin.tie(0);return 0;})();
