4af88abf4032236fb6ed33ef9e0eb16431ea6891
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/0148_word_pattern.cpp

--- platforms/leetcode/0148_word_pattern.cpp
+++ platforms/leetcode/0148_word_pattern.cpp
@@ -1,34 +1,25 @@
 #include "../../template.hpp"
 
 int sol(const string& pattern, const string& s) {
-    vector<string> words;
-    istringstream is(s);
+    const int n = s.size();
     string word;
-    while (is >> word) {
-        words.push_back(word);
-    }
-
-    if (pattern.size() != words.size()) return 0;
-
-    const int n = pattern.size();
-    unordered_map<string, char> m;
-    unordered_set<char> ussigned;
-    for (int i = 0; i < n; ++i) {
-        if (not m.count(words[i])) {
-            if (ussigned.count(pattern[i])) {
-                return 0;
-            }
-            m[words[i]] = pattern[i];
-            ussigned.insert(pattern[i]);
+    int idx = 0;
+    unordered_map<string, char> dict;
+    array<int, 26> used{};
+    for (int i = 0; i <= n; ++i) {
+        if (i < n and s[i] != ' ') {
+            word += s[i];
+            continue;
         }
-        else {
-            if (m[words[i]] != pattern[i]) {
-                return 0;
-            }
-        }
-    }
 
-    return 1;
+        if (dict.count(word) and pattern[idx] != dict[word]) return false;
+        if (not dict.count(word) and used[pattern[idx] - 'a']) return false;
+
+        used[pattern[idx] - 'a'] = 1;
+        dict.insert({word, pattern[idx++]});
+        word.clear();
+    }
+    return idx == pattern.size();
 }
 
 int main() { TimeMeasure _; __x();
@@ -36,4 +27,5 @@ int main() { TimeMeasure _; __x();
     cout << sol("abba", "dog cat cat fish") << endl; // 0
     cout << sol("aaaa", "dog cat cat dog") << endl; // 0
     cout << sol("abba", "dog dog dog dog") << endl; // 0
+    cout << sol("jquery", "jquery") << endl; // 0
 }
