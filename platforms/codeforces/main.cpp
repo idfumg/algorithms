08ce04d112f61b891e006947ac55eea5115a244d
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:23:21 +0300

    Add platforms/codeforces/main.cpp

---
+++ platforms/codeforces/main.cpp
@@ -0,0 +1,31 @@
+//#include "../../template.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+//constexpr int INF = 1000000000 + 7;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+
+void sol(istream& is = std::cin) {
+
+}
+
+void sol(const string& input) {
+    istringstream is(input);
+    sol(is);
+    cout << endl;
+}
+
+int main() { //TimeMeasure _; __x();
+    static const string input1 = R"(
+5 2
+1 2
+2 3
+3 4
+2 5
+)";
+
+    sol(input1); // 4
+    //sol(std::cin);
+}
