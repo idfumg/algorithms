f9d1aced8796912de512be7bd44526a981c9bc74
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/main.cpp

--- platforms/leetcode/main.cpp
+++ platforms/leetcode/main.cpp
@@ -1,6 +1,7 @@
 #include "../../template.hpp"
 
 
-int main() { TimeMeasure _; __x();
+const int fastio_ = ([](){std::ios_base::sync_with_stdio(0); std::cin.tie(0);return 0;})();
+int main() { TimeMeasure _;
 
 }
