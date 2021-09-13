6216e19ee5330ffd55b63390d1c534c8ebb38785
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/0003_increasing_triplet_subsequence.cpp

--- platforms/leetcode/0003_increasing_triplet_subsequence.cpp
+++ platforms/leetcode/0003_increasing_triplet_subsequence.cpp
@@ -1,24 +1,27 @@
 #include "../../template.hpp"
 
-int tab(const vi& arr) {
+int sol(const vi& arr) {
     const int n = arr.size();
-
     deque<int> mq;
     vi left(n, -1);
     for (int i = 0; i < n; ++i) {
-        while (not mq.empty() and arr[i] <= arr[mq.back()]) mq.pop_back();
-        if (not mq.empty()) left[i] = mq.back();
+        while (not mq.empty() and arr[i] <= arr[mq.back()]) {
+            mq.pop_back();
+        }
+        if (mq.empty()) left[i] = -1;
+        else left[i] = mq.back();
         mq.push_back(i);
     }
-
     mq.clear();
     vi right(n, n);
     for (int i = n - 1; i >= 0; --i) {
-        while (not mq.empty() and arr[i] >= arr[mq.back()]) mq.pop_back();
-        if (not mq.empty()) right[i] = mq.back();
+        while (not mq.empty() and arr[i] >= arr[mq.back()]) {
+            mq.pop_back();
+        }
+        if (mq.empty()) right[i] = n;
+        else right[i] = mq.back();
         mq.push_back(i);
     }
-
     for (int i = 1; i < n - 1; ++i) {
         if (left[i] != -1 and right[i] != n) {
             return 1;
@@ -28,10 +31,12 @@ int tab(const vi& arr) {
 }
 
 int main() { TimeMeasure _; __x();
-    cout << tab({1,2,3,4,5}) << endl; // 1
-    cout << tab({5,4,3,2,1}) << endl; // 0
-    cout << tab({2,1,5,0,4,6}) << endl; // 1
-    cout << tab({20,100,10,12,5,13}) << endl; // 1
-    cout << tab({1,2,1,3}) << endl; // 1
-    cout << tab({1,5,0,4,1,3}) << endl; // 1
+    cout << sol({1,2,3,4,5}) << endl; // 1
+    cout << sol({5,4,3,2,1}) << endl; // 0
+    cout << sol({2,1,5,0,4,6}) << endl; // 1
+    cout << sol({20,100,10,12,5,13}) << endl; // 1
+    cout << sol({1,2,1,3}) << endl; // 1
+    cout << sol({1,5,0,4,1,3}) << endl; // 1
+    cout << sol({2,4,-2,-3}) << endl; // 0
+    cout << sol({1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}) << endl; // 0
 }
