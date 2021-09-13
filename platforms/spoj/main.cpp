a8f2d043ee77396d23ecd202fc068601e4d4d592
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:22:32 +0300

    Add platforms/spoj/main.cpp

---
+++ platforms/spoj/main.cpp
@@ -0,0 +1,25 @@
+//#include "../../template.hpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol(istream& is = std::cin) {
+
+}
+
+void sol(const string& input) {
+    istringstream is(input);
+    sol(is);
+    cout << endl;
+}
+
+int main() { //TimeMeasure _; __x();
+    static const string input1 = R"(
+
+)";
+    sol(input1);
+}
+
+// int main() {
+//     sol(std::cin);
+// }
