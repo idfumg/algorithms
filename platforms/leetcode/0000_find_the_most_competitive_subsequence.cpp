901a07f96626e0edf371e02f0700bfdc000e944a
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/0000_find_the_most_competitive_subsequence.cpp

--- platforms/leetcode/0000_find_the_most_competitive_subsequence.cpp
+++ platforms/leetcode/0000_find_the_most_competitive_subsequence.cpp
@@ -1,23 +1,21 @@
 #include "../../template.hpp"
 
-vi tab(const vi& arr, int k) {
-    int n = arr.size();
+vi sol(vi elems, int k) {
+    int n = elems.size();
     deque<int> mq;
     for (int i = 0; i < n; ++i) {
-        while (not mq.empty() and arr[i] < arr[mq.back()] and mq.size() + n - i - 1 >= k) {
+        while (not mq.empty() and elems[i] < mq.back() and mq.size() - 1 + n - i >= k) {
             mq.pop_back();
         }
-        mq.push_back(i);
+        mq.push_back(elems[i]);
     }
-    vi res(k, 0);
-    for (int i = 0; i < k; ++i) {
-        res[i] = arr[mq[i]];
-    }
-    return res;
+    while (mq.size() > k) mq.pop_back();
+    return vi(mq.begin(), mq.end());
 }
 
 int main() { TimeMeasure _; __x();
-    cout << tab({3,5,2,6}, 2) << endl; // 2,6
-    cout << tab({2,4,3,3,5,4,9,6}, 4) << endl; // 2,3,3,4
-    cout << tab({71,18,52,29,55,73,24,42,66,8,80,2}, 3) << endl; // 8, 80, 2
+    cout << sol({3,5,2,6}, 2) << endl; // 2,6
+    cout << sol({2,4,3,3,5,4,9,6}, 4) << endl; // 2,3,3,4
+    cout << sol({71,18,52,29,55,73,24,42,66,8,80,2}, 3) << endl; // 8, 80, 2
+    cout << sol({1,1,1,1,1,1,1,1,0,0}, 5) << endl; // 1,1,1,0,0
 }
