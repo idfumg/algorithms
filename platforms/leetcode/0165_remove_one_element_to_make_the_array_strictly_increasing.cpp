0bfca47baf185f24c1788660f9b7405458a61e61
Artem Pushkin <idfumg@gmail.com>
2021-09-13 20:24:44 +0300

    Update platforms/leetcode/0165_remove_one_element_to_make_the_array_strictly_increasing.cpp

--- platforms/leetcode/0165_remove_one_element_to_make_the_array_strictly_increasing.cpp
+++ platforms/leetcode/0165_remove_one_element_to_make_the_array_strictly_increasing.cpp
@@ -1,18 +1,34 @@
 #include "../../template.hpp"
 
-int sol(const vi& arr, const int n, const int k) {
-    if (n <= 0) return k >= 0;
-    if (arr[n] <= arr[n - 1]) {
-        if (n == 1) return k > 0;
-        if (arr[n] > arr[n - 2] and sol(arr, n - 2, k - 1)) return true;
-        if (n < arr.size() - 1 and arr[n + 1] > arr[n - 1] and sol(arr, n - 1, k - 1)) return true;
-        return n == arr.size() - 1 and sol(arr, n - 1, k - 1);
-    }
-    return sol(arr, n - 1, k);
+int sol(vi nums, int n, int k) {
+    if (k < 0) return 0;
+    if (n <= 1) return 1;
+    if (n == 2) return nums[n - 1] > nums[n - 2] or k > 0;
+    if (nums[n - 1] <= nums[n - 2]) return (nums[n - 2] < nums[n] and sol(nums, n - 1, k - 1)) or (nums[n - 1] > nums[n - 3] and sol(nums, n - 2, k - 1));
+    return sol(nums, n - 1, k);
 }
 
-int sol(const vi& arr) {
-    return sol(arr, arr.size() - 1, 1);
+int sol(vi nums) {
+    int n = nums.size();
+    if (n == 1) return 1;
+    if (nums[n - 1] <= nums[n - 2]) return sol(nums, n - 1, 0) or (nums[n - 1] > nums[n - 3] and sol(nums, n - 2, 0));
+    return sol(nums, n - 1, 1);
+}
+
+int sol2(vi nums) {
+    int n = nums.size();
+    vi dp(n, 1);
+    dp[0] = 1;
+    int max_length = 1;
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (nums[i] > nums[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        max_length = max(max_length, dp[i]);
+    }
+    return (n - max_length) <= 1;
 }
 
 int main() { TimeMeasure _; __x();
@@ -24,4 +40,13 @@ int main() { TimeMeasure _; __x();
     cout << sol({512,867,904,997,403}) << endl; // 1
     cout << sol({100,21,100}) << endl; // 1
     cout << sol({915,444,500}) << endl; // 1
+    cout << endl;
+    cout << sol2({1,2,10,5,7}) << endl; // 1
+    cout << sol2({2,3,1,2}) << endl; // 0
+    cout << sol2({1,1,1}) << endl; // 0
+    cout << sol2({1,2,3}) << endl; // 1
+    cout << sol2({105,924,32,968}) << endl; // 1
+    cout << sol2({512,867,904,997,403}) << endl; // 1
+    cout << sol2({100,21,100}) << endl; // 1
+    cout << sol2({915,444,500}) << endl; // 1
 }
