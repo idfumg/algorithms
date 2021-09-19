59219bc385619947f340b39b262ab51b3b59264a
Artem Pushkin <idfumg@gmail.com>
2021-09-19 19:20:19 +0300

    Update platforms/leetcode/main.cpp

--- platforms/leetcode/main.cpp
+++ platforms/leetcode/main.cpp
@@ -1,7 +1,11 @@
 #include "../../template.hpp"
 
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+//const i32 INF = 1000000000 + 7;
+const i32 fastio_ = ([](){std::ios_base::sync_with_stdio(0); std::cin.tie(0);return 0;})();
 
-const int fastio_ = ([](){std::ios_base::sync_with_stdio(0); std::cin.tie(0);return 0;})();
+
 int main() { TimeMeasure _;
 
 }
